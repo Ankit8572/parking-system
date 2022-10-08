c4d7caea7dd306109595f2c7b92e710d64485638
Ankit Kumar <100014751+Ankit8572@users.noreply.github.com>
2022-10-08 21:35:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// const toggleSwitch = document.querySelector('.theme-switch input[type = "checkbox"]');
+const toggleSwitch = document.querySelector('.theme-switch input[type = "checkbox"]');
 
-// function switchTheme(e){
-//     if(e.target.checked){
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//     }
-//     else{
-//         document.documentElement.setAttribute('data-theme', 'Light');
-//     }
-// }
+function switchTheme(e){
+     if(e.target.checked){
+         document.documentElement.setAttribute('data-theme', 'dark');
+     }
+     else{
+         document.documentElement.setAttribute('data-theme', 'Light');
+     }
+ }
 
-// toggleSwitch.addEventListener('change', switchTheme, false);
+ toggleSwitch.addEventListener('change', switchTheme, false);
 
 const name = document.getElementById('name');
 const email = document.getElementById('email');
@@ -41,12 +41,12 @@ if(massage.value.lenght < 20){
 errorElement.innerHTML =' ';
 successMsg.innerHTML ='Thanks you! I will get back to you as soon as possible';
 
-// e.preventDefault();
-// setTimeout(function () {
-//     successMsg.innerHTML= ' ';
-//     document.getElementById('contact-form').reset();
-// }, 6000);
-// return false;
+ e.preventDefault();
+ setTimeout(function () {
+     successMsg.innerHTML= ' ';
+     document.getElementById('contact-form').reset();
+ }, 6000);
+ return false;
 }
 
 const emailISValid = email=> { 
